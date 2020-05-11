3dc9b20ee630430a559f675f7ac908b3d27107fb
mario robayo <marioferyhwh@gmail.com>
2020-05-11 07:51:24 -0500

    Correcion d ecabezera cors

--- app.js
+++ app.js
@@ -14,16 +14,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // cors
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Oring", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Request-with, Content-Type, Accept, Access-control-Allow-Request-Method"
+    "Authorization, X-API-KEY, Origin, X-Requested-with, Content-Type, Accept, Access-control-Allow-Request-Method"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
 
   next();
 });
+
 // prefiijos alas rutas
 
 //añadir prefijo de rutas
